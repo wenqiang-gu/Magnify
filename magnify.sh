1605a316eadb663f7d9861ab959435eb6f8418a7
Brett Viren <brett.viren@gmail.com>
2017-08-24 16:54:33 -0400

    Allow loading ROOT file directly from HTTP.

--- magnify.sh
+++ magnify.sh
@@ -2,11 +2,21 @@
 
 magnify_source="$(dirname $(readlink -f $BASH_SOURCE))"
 
-rootfile=$(readlink -f $1) ; shift
+rootfile="$1" ; shift
+if [[ "$rootfile" =~ :// ]] ; then
+    echo "Loading URL $rootfile"
+else
+    rootfile=$(readlink -f "$rootfile")
+fi
 frame="${1:-decon}"
 startdir=$(pwd)
 
 echo "Loading frame \"$frame\""
 
 cd $magnify_source/scripts
+
+echo $rootfile
+echo $frame
+
+
 root -l loadClasses.C Magnify.C'("'"$rootfile"'","'"$frame"'")'
